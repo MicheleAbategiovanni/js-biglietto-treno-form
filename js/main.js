cf48f6a6cec74646a05ebaa3d3056bbeb72f9933
MicheleAbategiovanni <michele.abategiovanni@gmail.com>
2022-10-28 15:27:28 +0200

    completed input requests

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 const nameEl = document.querySelector("#name");
+const lastNameEl = document.querySelector("#lastName");
+const ageEl = document.querySelector("#ageUser");
 const distanceEl = document.querySelector("#distance");
+
 const buyTicket = document.querySelector(".btn");
 
 
 nameEl.value;
+lastNameEl.value;
+ageEl.value;
 distanceEl.value;
+
 
 buyTicket.addEventListener("click", function () {
 
     console.log("Hai inserito come Nome: ", nameEl.value);
-    // console.log("Hai inserito come Cognome: ", nameEl.value);
-    // console.log("Hai inserito come Età: ", nameEl.value);
+    console.log("Hai inserito come Cognome: ", lastNameEl.value);
+    console.log("Hai inserito come Età: ", ageEl.value);
     console.log("Hai inserito come Distanza: ", distanceEl.value, "km");
     
 
