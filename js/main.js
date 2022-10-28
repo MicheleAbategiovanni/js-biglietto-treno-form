7bfde996ff63a9ce639715ee3bc77e516c7e7ee2
MicheleAbategiovanni <michele.abategiovanni@gmail.com>
2022-10-28 14:58:19 +0200

    project initialization

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const inputEl = document.querySelector("input");
+const distanceEl = document.querySelector("#distance");
+const buyTicket = document.querySelector(".btn");
+
+
+inputEl.value;
+distanceEl.value;
+
+buyTicket.addEventListener("click", function () {
+
+    console.log("Hai inserito come età: ", inputEl.value);
+    console.log("Hai inserito come distnza: ", distanceEl.value);
+    
+
+
+});
+
+
+
+//   console.log( inputEl.value );
